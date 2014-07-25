085ff8d803d6c6b631f237afd32ca3c47666cd0a
gsusmobile <gsnagel@gmail.com>
2014-07-24 20:44:04 -0400

    Updating about page.

---
+++ main.py
@@ -0,0 +1,12 @@
+import os
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/about.html')
+def about():
+    return render_template('about.html')
+
+@app.route('/')
+def root():
+    return render_template('root.html')
